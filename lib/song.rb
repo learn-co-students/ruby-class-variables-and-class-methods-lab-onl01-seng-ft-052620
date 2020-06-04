9658a73869607099798a293ba34e23dfd40d40a1
JohnAjaka <johnajaka96@gmail.com>
2020-06-03 22:53:21 -0400

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,43 @@
+class Song
+
+    attr_accessor :name, :artist, :genre
+    @@count = 0
+    @@genres = []
+    @@artists = []
+    def initialize(name, artist, genre)
+        @name = name
+        @artist = artist
+        @genre = genre
+        @@count += 1
+        @@genres << genre
+        @@artists << artist
+    end
+
+    def self.genre_count
+        genre_count = Hash.new(0)
+        @@genres.each do |genre|
+            genre_count[genre] +=1
+        end
+        genre_count
+    end
+
+    def self.artist_count
+        artist_count = Hash.new(0)
+        @@artists.each do |name|
+            artist_count[name] += 1
+        end
+        artist_count
+    end
+
+    def self.artists
+        @@artists.uniq
+    end
+
+    def self.genres
+        @@genres.uniq
+    end
+
+    def self.count
+        @@count
+    end
+end
