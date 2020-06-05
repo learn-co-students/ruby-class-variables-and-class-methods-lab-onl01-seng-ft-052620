863172f6964b0dd66548a27792782bc96ea0123a
Eyerusalem K <3yerus@gmail.com>
2020-06-05 21:24:42 +0000

    Done.

---
+++ lib/song.rb
@@ -0,0 +1,54 @@
+class Song 
+ 
+attr_accessor :name, :artist, :genre
+
+@@count = 0
+@@artists = []
+@@genres = []
+def initialize(name, artist, genre)
+  
+  @name = name
+ @artist = artist
+  @genre = genre
+  @@count +=1
+  @@artists << @artist
+ @@genres << @genre
+end
+
+def self.count 
+@@count
+end
+
+def self.artists
+  @@artists.uniq
+end 
+
+
+def self.genres
+  
+  @@genres.uniq
+end 
+
+def self.genre_count 
+genre_count = {}
+
+self.genres.each do |genre|
+genre_count[genre] = @@genres.count {|g| g == genre}
+
+end
+genre_count
+end 
+
+
+def self.artist_count
+artist_count = {}
+
+self.artists.each do |artist|
+artist_count[artist] = @@artists.count {|a| a == artist}
+
+end
+artist_count
+end 
+
+end
+
